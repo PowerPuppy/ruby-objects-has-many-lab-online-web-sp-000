692d421d186ac75e795a790ae44ab83b34a33ad9
Chris Cheong <chrisxcheong@gmail.com>
2019-07-31 00:14:52 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,30 @@
-class Author
-attr_accessor :name
 
-def initalize(name)
-  @name = name
-end
+class Author
+
+  @@post_count = []
+
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author =  self
+    @@post_count << self
+  end
+
+  def add_post_by_title(name)
+    new_post = Post.new(name)
+    add_post(new_post)
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+
 
 end
